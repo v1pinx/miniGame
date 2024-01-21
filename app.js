152ef9d8fa427a5506861f5c9a90fdc703f838e4
Vipin Kumar <vkprajapati665@gmail.com>
2024-01-21 14:29:00 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,85 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+//playerX, playerO
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [2, 4, 6],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("Box was clicked");
+        if(turnO){
+            box.innerHTML = "O";
+        }
+        else{
+            box.innerHTML = "X";
+        }
+        box.disabled = true;
+        turnO = !turnO;
+
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText ="";
+    }
+}
+
+const showWinner = (Winner) => {
+    msg.innerText = `Congratulations, Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos3Val === pos2Val){
+                console.log("Winner", pos1Val);
+                showWinner(pos1Val);
+                disableBoxes();
+            }
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+/*
+    implement the "Draw" Condition
+
+*/
